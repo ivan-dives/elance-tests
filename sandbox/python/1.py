5d84d364b445c9353a2fc32ef287c04549bb1f57
Ivan Diorditsa <ivan.diorditsa@gmail.com>
2013-12-04 12:47:37 +0200

    sandbox/python: use generators instead of lists

--- sandbox/python/1.py
+++ sandbox/python/1.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 
-print ','.join([str(x) for x in xrange(1001, 1200, 7) if x % 5 != 0])
+print ','.join((str(x) for x in xrange(1001, 1200, 7) if x % 5 != 0))
 
--- sandbox/python/3.py
+++ sandbox/python/3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-balance = sum([int(money) * (1 if _class == 'D' else -1) for money, _class in [transaction.split('-') for transaction in raw_input().split(',')]])
+balance = sum((int(money) * (1 if _class == 'D' else -1) for money, _class in (transaction.split('-') for transaction in raw_input().split(','))))
 if balance > 5000:
 	balance *= 1.05
 print int(balance)
